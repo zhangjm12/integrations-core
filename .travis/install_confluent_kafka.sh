e80b4bde318e636d33341ac1d6243e7b1d9d78d0
Julia Simon <611228+hithwen@users.noreply.github.com>
2019-08-22 14:16:49 +0200

    Add --allow-unauthenticated to mapr client install

--- .travis/install_confluent_kafka.sh
+++ .travis/install_confluent_kafka.sh
@@ -17,7 +17,7 @@ fi
 sudo add-apt-repository 'deb http://package.mapr.com/releases/v6.1.0/ubuntu binary trusty'
 sudo add-apt-repository 'deb http://package.mapr.com/releases/MEP/MEP-6.1.0/ubuntu binary trusty'
 sudo apt-get update
-sudo apt-get install mapr-client -y
+sudo apt-get install mapr-client -y --allow-unauthenticated
 #wget
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/mapr/lib #:/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.222.b10-0.el7_6.x86_64/jre/lib/amd64/server/
 /opt/datadog-agent/embedded/bin/python2 -m pip install confluent_kafka
