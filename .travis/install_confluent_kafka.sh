017537aeb4047fb5936321a154d24229d13b6119
Julia Simon <611228+hithwen@users.noreply.github.com>
2019-08-22 14:02:16 +0200

    Add konfluent kafka installation to travis

---
+++ .travis/install_confluent_kafka.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -ex
+
+if [ -z "$CHECK" ]; then
+    OUT=$(ddev test --list)
+    if [[ "$OUT" != *"mapr"* ]]; then
+        exit 0
+    fi
+else
+    if [ $CHECK != "mapr" ]; then
+        exit 0
+    fi
+fi
+
+
+sudo apt-get update
+sudo apt-get install mapr-client
+#wget
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/mapr/lib #:/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.222.b10-0.el7_6.x86_64/jre/lib/amd64/server/
+/opt/datadog-agent/embedded/bin/python2 -m pip install confluent_kafka
+/opt/datadog-agent/embedded/bin/python3 -m pip install confluent_kafka
+
+set +ex
