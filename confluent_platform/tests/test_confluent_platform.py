1761b05cc022239b3f71b92a8b27f65364552b8d
Alexandre Yang <alexandre.yang@datadoghq.com>
2020-02-03 14:41:17 +0100

    Add scaffolding

---
+++ confluent_platform/tests/test_confluent_platform.py
@@ -0,0 +1,11 @@
+# (C) Datadog, Inc. 2020-present
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+from datadog_checks.confluent_platform import ConfluentPlatformCheck
+
+
+def test_check(aggregator, instance):
+    check = ConfluentPlatformCheck('confluent_platform', {}, {})
+    check.check(instance)
+
+    aggregator.assert_all_metrics_covered()
