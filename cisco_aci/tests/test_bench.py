6a85143a60ba2d1c6ed6a7dec039116d2b49a2a4
ChristineTChen <christine.chen@datadoghq.com>
2020-02-05 17:29:32 -0500

    Comment out benchmark

--- cisco_aci/tests/test_bench.py
+++ cisco_aci/tests/test_bench.py
@@ -1,7 +1,20 @@
-import time
+# C Datadog, Inc. 2020-Present
+# All rights reserved
+# Licensed under Simplified BSD License see LICENSE
 
 from datadog_checks.cisco_aci import CiscoACICheck
-from .conftest import SANDBOX_CONFIG
+
+SANDBOX_CONFIG = {
+    'instances': [
+        {
+            'aci_url': 'https://sandboxapicdc.cisco.com',
+            'username': 'admin',
+            'pwd': 'ciscopsdt',
+            'tls_verify': False,  # As of 2019-12-03, the sandbox endpoint doesn't have a valid TLS certificate.
+        }
+    ]
+}
+
 
 def test_check(benchmark):
     ciscocheck = CiscoACICheck('cisco_aci', {}, SANDBOX_CONFIG)
